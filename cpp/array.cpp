75d0128efd7503b7856a77b20b5694226be6dc3e
Asami Tachibana <atachibana2612@gmail.com>
2017-10-18 16:12:05 -0400

    Nonsense!

---
+++ cpp/array.cpp
@@ -0,0 +1,17 @@
+/*
+
+  array.cpp
+  "Learn How To Do Arrays, With the Power of UNIX!"
+  18 October 2017
+
+  (c) Robert Maloy
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  cout << argv[1];
+}
