80af278f077a326534715c7a0b96ca4ca213d947
Asami Tachibana <atachibana2612@gmail.com>
2017-11-01 10:36:26 -0400

    Added by auto-script

---
+++ cpp/motd/main.cpp
@@ -0,0 +1,16 @@
+/*
+MOTD generator
+(c) Robert Maloy
+31 October 2017
+*/
+
+#include "asc.h"
+#include "randomnum.h"
+#include "scrambler.h"
+using namespace std;
+
+int main()
+{
+  ASC::getFileContents("ascii.txt");
+  return 0;
+}
