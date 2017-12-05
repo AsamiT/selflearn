3ef285ed1b5e849680ebc47c250bd814a8defffe
Asami Tachibana <atachibana2612@gmail.com>
2017-12-05 10:05:12 -0500

    Added by auto-script

--- bashassn/userScript.bash
+++ bashassn/userScript.bash
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash'
+
+set -x
 
 if (( $# < 1 ))
 then
@@ -13,16 +15,16 @@ main_function() {
   echo "I can't get no... satisfaction..."
 }
 
-if [[ -l $TERM ]]; then
-  main_function 2>&1 >> /var/log/userScript_log.log
-else
-  main_function
-fi
+# if [ -l $TERM ]; then
+#   main_function 2>&1 >> /var/log/userScript_log.log
+# else
+#   main_function
+# fi
 
-if [[ -v $TERM ]]; then
+if [ -v $TERM ]; then
   echo "Karma Chameleon."
 fi
 
-if [[ -d $TERM ]]; then
+if [ -d $TERM ]; then
     neofetch //simply execute neofetch.
 fi
