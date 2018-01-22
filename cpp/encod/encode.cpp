5f2cf915e9ef38f67a669de43688c83061bcb70d
Asami Tachibana <atachibana2612@gmail.com>
2018-01-21 21:22:10 -0500

    Added by auto-script

---
+++ cpp/encod/encode.cpp
@@ -0,0 +1,16 @@
+
+/* encode.cpp */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+  string str;
+  cout << "Please enter a string: " << endl;
+  getline (cin,str);
+  cout << str << endl;
+
+
+}
