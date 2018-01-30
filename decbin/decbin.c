0a8f982b14d64b4649842df443ad745cad856d16
Asami Tachibana <atachibana2612@gmail.com>
2018-01-30 11:45:18 -0500

    Added by auto-script

---
+++ decbin/decbin.c
@@ -0,0 +1,17 @@
+/*
+
+  decbin.c
+  "In a village in Old Siam, Sir"
+  30 January 2018
+
+*/
+
+#include "includes.h"
+
+int main()
+{
+  int dec;
+  printf("Please enter a decimal value to convert to binary: ");
+  scanf("%d", &dec);
+
+}
