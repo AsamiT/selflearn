b2593d0688dead28cacb54013fecbcc6d65ae623
Asami Tachibana <atachibana2612@gmail.com>
2018-01-30 11:50:10 -0500

    Added by auto-script

--- decbin/decbin.c
+++ decbin/decbin.c
@@ -10,8 +10,21 @@
 
 int main()
 {
-  int dec;
+  int dec, num_bin_dig;
   printf("Please enter a decimal value to convert to binary: ");
   scanf("%d", &dec);
+
+  char** binout;
+  binout = malloc((num_bin_dig)*sizeof(char*));
+
+  if (binout == NULL) {
+    printf("ERROR: No bits!\n");
+    return 0;
+  }
+
+  for (i = 0; dec != 0; i++) {
+    num_bin_dig += 1;
+    dec = dec/2;
+  }
 
 }
