d8f78adc8d77303683485c5cef65152e5c1a41f0
Asami Tachibana <atachibana2612@gmail.com>
2018-02-03 20:18:53 -0500

    Added by auto-script

---
+++ Project 1/IrmaMoves.c
@@ -0,0 +1,13 @@
+/*
+
+  IrmaMoves.c
+  3 February 2018
+  Robert Maloy
+
+  */
+
+char **createMapBoard(void) {
+  int a=8, b=8;
+  char *arr = (char *)malloc(a*b*sizeof(char));
+  strncpy(board[0], "FF ", 8);
+}
