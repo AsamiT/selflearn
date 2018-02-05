70467ab1aa878cfad959251b603b9d12ed493f2c
Asami Tachibana <atachibana2612@gmail.com>
2018-02-05 09:47:19 -0500

    Added by auto-script

--- Project 1/IrmaMoves.c
+++ Project 1/IrmaMoves.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc/malloc.h> //macOS debug include. Does not work under Microsoft Windows or Linux.
 #define ROW 8
 #define COL 8
 
@@ -23,26 +24,32 @@ void block() {
 
 char **createMapBoard(void) {
     
-    static char *board[ROW];
+    static char *board[ROW]; //static cast because otherwise we can't return it to the original function/main.
     for (int i=0; i<ROW; i++) {
         board[i] = (char *)malloc(COL * sizeof(char));
     }
     
     if (*board == NULL)
     {
-        printf("malloc failed!\n");
+        printf("malloc failed!\n"); //fairly obvious.
         exit(1);
     }
     
-    for (int a=0; a<ROW; a++) {
+    for (int a=0; a<ROW; a++) { //40 days and 40 nights of rainfall
         for (int b=0; b<COL; b++) {
-            strlcpy(&board[a][b], " ", 8);
+            strcpy(&board[a][b], " ");
         }
     }
     
+    printf("Memory allocated for board: %zu\n", malloc_size(*board));
+    for (int i=0; i<COL; i++) {
+        for (int z=0; z<ROW; z++) {
+            strcpy(&board[i][z], "Test");
+        }
+    }
     
-    for (int z=0; z<2; z++) { copy F values onto board
-    
+    /* for (int z=0; z<2; z++) { //copy F values onto board
+        //strlcpy(&board[0][z], "F", 8);
         if (z==1) {
             strlcpy(&board[z][z], "F", 8);
             strlcpy(&board[z+1][z], "F", 8);
@@ -50,9 +57,9 @@ char **createMapBoard(void) {
             strlcpy(&board[z+2][z+1], "F", 8);
         }
     }
-    strlcpy(&board[4][3], "K", 8); here we copy the individual K and individual B ones
+    strlcpy(&board[4][3], "K", 8); //here we copy the individual K and individual B ones
     strlcpy(&board[5][4], "B", 8);
-    for (int z=0; z<=2; z++) { this loop pushes C values to the array
+    for (int z=0; z<=2; z++) { //this loop pushes C values to the array
         if(z==0) {
             strlcpy(&board[5][z], "C", 8);
         }
@@ -65,7 +72,7 @@ char **createMapBoard(void) {
         }
     }
     
-    for (int z=5; z<8; z++) {
+    for (int z=5; z<8; z++) { //establish the island of Dominica
         if (z==5) {
             strlcpy(&board[6][z], "D", 8);
         }
@@ -73,18 +80,18 @@ char **createMapBoard(void) {
             strlcpy(&board[7][z], "D", 8);
             strlcpy(&board[7][z+1], "D", 8);
         }
-    }
+    } */
     
     return board;
     
 }
 
-/*
 char **destroyMapBoard(char **board) {
-    printf("memory allocated for board: %d", malloc_size(board));
+    printf("memory allocated for board: %zu", malloc_size(board));
     free(board);
-    printf("memory allocated for board: %d", malloc_size(board));
-} */
+    printf("memory allocated for board: %zu", malloc_size(board));
+    return NULL;
+}
 
 void printMapBoard(char **board) {
     block();
